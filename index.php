e32a16549354435ecb94c988919f0b25cbb43909
AnarchyTeam <36066276+AnarchyTeam@users.noreply.github.com>
2018-02-04 17:39:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
 require_once('./line_class.php');
 require_once('./unirest-php-master/src/Unirest.php');
 
-$channelSecret = 'd735bbd21936b39f05224829eaed6f50'; //sesuaikan
-$channelAccessToken = 'KSqC4L4DQB5o2uk3eZTIwSNQgUGKoMF451X9VIkgmlzzDTEw+yCoA1eknDJ8HQM/IK0aLhJYABpYBZZpInVA7tksnVvLen2MUIDGwR8MtG1DBnz9LFcip99gZJ0zqCaezrR3tQlGlK8Xhb7Gz3nPMAdB04t89/1O/w1cDnyilFU='; //sesuaikan
+$channelSecret = 'c41d451e0f42f191c338b27603879566'; //sesuaikan
+$channelAccessToken = 'k/+WdpkSoZof/xgLClTWtxKAI/MqqJEwyB3Rjydp5jbAUMSVR/PELhxjbsJI9dwSbVLRX/f1gzhhDJ14cIIqBkD9XTrPhF8ecmdUqfw8p884zghRxnMVvZvbDP0ItLB11LLep1Ye/wmwT/uJXVNo1QdB04t89/1O/w1cDnyilFU='; //sesuaikan
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
